8bdf34a44e85ed9087c1bfe4ab4cdeb238076066
tusharawat01 <tushar0402tr@gmail.com>
2024-04-29 16:39:40 +0530

    spotlight and moving button ui in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection"
 
 export default function Home() {
   return (
-    <h1 text-centre mt-8>Hello World!</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+    </main>
+
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,9 +17,7 @@ function Navbar({ className }: { className?: string }) {
 
                     </MenuItem>
                 </Link>
-                <MenuItem
-                    setActive={setActive} active={active} item="Our Courses"
-                >
+                <MenuItem setActive={setActive} active={active} item="Our Courses">
                     <div className="flex flex-col space-y-4 text-sm">
                         <HoveredLink href="/courses">All Courses</HoveredLink>
                         <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
