a3d44edc54f286e4bceebb4e7ba9abea7eba587c
tusharawat01 <tushar0402tr@gmail.com>
2024-04-30 16:58:32 +0530

    Welcome Page with effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection"
 import TestimonialCards from "@/components/TestimonialCards";
 import Webiners from "@/components/Webiners";
+import WelcomePage from "@/components/WelcomePage";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <WelcomePage />
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
